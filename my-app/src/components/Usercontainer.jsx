504ec320178a0cdc31f06eefdfbb67df20bfca1a
unknown <phamducthien19012006@gmail.com>
2022-06-05 09:09:55 +0700

    update

--- my-app/src/components/Usercontainer.jsx
+++ my-app/src/components/Usercontainer.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserItem from "./UserItem";
-import { pictureUsers } from "../constants/mock.js";
 import { Grid } from "@nextui-org/react";
+
 const Usercontainer = () => {
+    const [users, setUsers] = useState([])
+    const getUser = async () => {
+        const request = await fetch("https://reqres.in/api/users?page=2")
+        const response = await request.json()
+        const data = response.data
+        setUsers(data);
+    }
+    useEffect(() => {
+        getUser();
+    },[])
+    console.log("users",users)
   return (
     <Grid.Container gap={2}>
-      {pictureUsers.map((data) => {
+      {users.map((data) => {
         return (
           <Grid xs={4} key={data.id}>
             <UserItem  user={data} />
